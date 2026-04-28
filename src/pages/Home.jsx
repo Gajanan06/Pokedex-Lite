60d9a156d149bcf4e6f3d6b2951244708b619072
Gajanan06 <gajanannilajkar06@gmail.com>
2026-04-28 16:55:48 +0530

    Added search funtionality and improved UI

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,8 @@ import { getPokemonList } from "../services/pokemon";
 function Home() {
   const [pokemon, setPokemon] = useState([]);
 
+  const [searchTerm, setSearchTerm] = useState("");
+
   useEffect(() => {
     fetchPokemon();
   }, []);
@@ -13,12 +15,26 @@ function Home() {
     setPokemon(data);
   };
 
+  const filteredPokemon = pokemon.filter((poke) =>
+  poke.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div className="p-4">
       <h1 className="text-2xl font-bold mb-4">Pokemon List</h1>
 
+      <div className="mb-4">
+       <input
+        type="text"
+        placeholder="Search Pokémon..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        className="w-full max-w-md px-4 py-2 border rounded"
+       />
+      </div>
+
       <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
-        {pokemon.map((poke) => {
+        {filteredPokemon.map((poke) => {
           const id = poke.url.split("/")[6];
           const image = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
 
@@ -33,6 +49,10 @@ function Home() {
           );
         })}
       </div>
+
+      {filteredPokemon.length === 0 && (
+        <p className="mt-4">No Pokémon found</p>
+      )}
     </div>
   );
 }
